3ed22bf1feb397aee8804a7b89ede90406b0fe0e
George M <georgemargineanu20@gmail.com>
2025-12-08 14:55:47 +0200

    added BDG

---
+++ BDG/common/run_pipeline.py
@@ -0,0 +1,25 @@
+from gather_all import run_adreal_pipeline, get_correct_period
+from google.cloud import secretmanager
+
+def access_secret(secret_id, version_id="latest"):
+    client = secretmanager.SecretManagerServiceClient()
+    project_id = "ums-adreal-471711"  # your actual project id
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(name=name)
+    return response.payload.data.decode("UTF-8")
+
+def main():
+    username = access_secret("adreal-username")
+    password = access_secret("adreal-password")
+    # BDG competitors
+    parent_brand_ids = ["39892", "76032", "74235", "14321", "13255", "1230", "1227", "1228", "2845",
+                        "75026", "6795", "17086", "2353", "40564", "1332", "81289", "21927", "94974", "94306"]
+
+    df = run_adreal_pipeline(username, password, parent_brand_ids=parent_brand_ids)
+    print(f"Data fetched: {len(df)} rows")
+
+    period = get_correct_period()
+    df.to_csv(f"{period}_Adreal.csv", index=False)
+
+if __name__ == "__main__":
+    main()

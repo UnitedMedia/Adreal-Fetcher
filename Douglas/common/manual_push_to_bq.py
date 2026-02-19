d1f9f4e95e047019cf6114f274380ef6f8fc3265
George M <georgemargineanu20@gmail.com>
2026-02-19 16:57:38 +0200

    typo

--- Douglas/common/manual_push_to_bq.py
+++ Douglas/common/manual_push_to_bq.py
@@ -23,7 +23,7 @@ except ImportError:
         sys.exit(1)
 
 PROJECT_ID = "ums-adreal-471711"
-TABLE_ID = f"{PROJECT_ID}.Mega.DataImport"
+TABLE_ID = f"{PROJECT_ID}.Douglas.DataImport"
 
 def access_secret(secret_id, version_id="latest"):
     """Fetch a secret from GCP Secret Manager."""
